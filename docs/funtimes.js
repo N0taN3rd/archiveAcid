711e601406e268369c292b471a8163f9c4bd756c
N0taN3rd <n0tan3rd@gmail.com>
2017-05-18 17:26:39 -0400

    updated funtimes

--- docs/funtimes.js
+++ docs/funtimes.js
@@ -1,3 +1,3 @@
 var elem = document.getElementById('evalInject')
-elem.innerHTML = `<iframe id="theIframeEvaled" sandbox="allow-same-origin allow-scripts" src="http://www.cs.odu.edu/~jberlin/evalInjectedIframe.html"></iframe>`
+elem.innerHTML = `<iframe id="theIframeEvaled" src="http://www.cs.odu.edu/~jberlin/evalInjectedIframe.html"></iframe>`
 
